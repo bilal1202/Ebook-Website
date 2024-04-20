0a28d7cd5b755ace8ec01fd20cde2931a0c79a18
Muhammad Bilal <87284041+bilal1202@users.noreply.github.com>
2024-04-20 05:21:01 +0500

    completed header section part css part

---
+++ script/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+
+// to add event on element
+
+
+const addEventonelem=function(elem,type,callback){
+    if(elem.length > 1){
+        for(let i=0;i<elem.length; i++)
+        {
+            elem[i].addEventListener(type,callback)
+        }
+    }
+    else{
+        elem.addEventListener(type,callback);
+    }
+}
